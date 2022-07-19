5804e7df1750357cfa672c785e3ab3890a848416
vhivestate <garcia.valeria001@gmail.com>
2022-07-19 17:25:57 -0600

    user input

--- assets/app.js
+++ assets/app.js
@@ -2,16 +2,16 @@ function getInput() {
     var input = document.getElementsByTagName("input");
     var name = document.querySelector("input[name='name']").value;
     console.dir(name);
-    // console.log(input);
-    // window.alert('submitted');
-}
+    console.log(input);
+    window.alert('submitted');
 
-function getBday() {
+
+// function getBday() {
     var bday = document.querySelector("input[name='bday']").value;
     console.dir(bday);
-}
 
-function getId() {
+
+// function getId() {
     var id = document.querySelector("input[id='license']").id;
     var idCheck = document.querySelector("input[id='license']").checked;
     var passport = document.querySelector("input[id='passport']").id;
@@ -23,9 +23,9 @@ function getId() {
     console.dir(passport);
     console.dir(ppCheck);
     console.dir(idNumber);
-}
 
-function getArtist() {
+
+// function getArtist() {
     var artist0 = document.querySelector("input[name='Garrett']").id;
     var check0 = document.querySelector("input[name='Garrett']").checked;
 
@@ -36,9 +36,9 @@ function getArtist() {
     console.dir(check0);
     console.dir(artist1);
     console.dir(check1);
-}
 
-function getCertify() {
+
+// function getCertify() {
     var box1 = document.querySelector("input[name='box1']").labels[0].innerText;
     var check1 = document.querySelector("input[name='box1']").checked;
 
@@ -74,16 +74,15 @@ function getCertify() {
     console.dir(check6);
     console.dir(box7);
     console.dir(check7);
-}
 
-function getSign() {
+// function getSign() {
     var sign = document.querySelector("input[name='signature']").name;
     var signature = document.querySelector("input[name='signature']").value;
     var date = document.querySelector("input[name='date']").name;
     var dateInput = document.querySelector("input[name='date']").value;
 
     console.dir(date);
-    console.dir(dateInput);
+    console.dir(dateInput)
     console.dir(sign);
-    console.dir(signature);
+            console.dir(signature)
 }
